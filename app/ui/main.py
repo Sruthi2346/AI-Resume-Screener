85afc83b4c9445fb20681b9ebc7ff3c926627239
Sruthi <sruthisakthi23@gmail.com>
2026-07-05 16:08:46 +0530

    Fix backend endpoint URL

--- app/ui/main.py
+++ app/ui/main.py
@@ -6,7 +6,7 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-API_URL = os.getenv("API_URL", "http://127.0.0.1:8000")
+API_URL = os.getenv("API_URL", "https://ai-resume-screener-owgh.onrender.com")
 
 st.set_page_config(page_title="AI Resume Screener", layout="wide")
 
